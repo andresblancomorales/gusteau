c0f526ee5376dbbda73eaf6e275770940788988a
Andres Blanco <andres.blanco@othree.io>
2018-08-30 00:54:30 -0600

    Adding CORS support

--- src/index.js
+++ src/index.js
@@ -27,6 +27,11 @@ connection.then(error => {
 
 const app = express();
 app.use(express.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 let userRepository = new UserRepository();
 let userManagement = new UserManagement(userRepository, cryptography);
